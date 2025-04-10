1705dd752f8a78636db7d10553ee1923334b8d76
Atharva708 <atharvagour07@gmail.vom>
2025-04-10 07:48:17 +0530

    Initial Commit

---
+++ Slyther Byte/Slyther_ByteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slyther_ByteApp.swift
+//  Slyther Byte
+//
+//  Created by Atharva Gour on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Slyther_ByteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Slyther ByteTests/Slyther_ByteTests.swift
@@ -0,0 +1,17 @@
+//
+//  Slyther_ByteTests.swift
+//  Slyther ByteTests
+//
+//  Created by Atharva Gour on 4/10/25.
+//
+
+import Testing
+@testable import Slyther_Byte
+
+struct Slyther_ByteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
